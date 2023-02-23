d2900c90e3a6e460a7cec8d18d3af67ea7129db1
msmithivas <msmithivas@gmail.com>
2023-02-23 22:28:10 +0000

    Next commit Added sqrt feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,4 +27,27 @@ def calculate_random
   render({ :template => "random_results.html.erb"})
 end
 
+def blank_sqrt
+  render({ :template => "sqrt_form.html.erb"})
+
+end
+
+def calculate_sqrt
+  @num = params.fetch("calc_value").to_f
+  @sqrt_of_num = @num ** 0.5 
+  render({ :template => "sqrt_results.html.erb"})
+
+end
+
+def blank_payment
+  render({ :template => "sqrt_form.html.erb"})
+
+end
+
+def calculate_payment
+  render({ :template => "sqrt_results.html.erb"})
+
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get("/random/new", { :controller => "application", :action => "blank_random" })
   get("/random/results", { :controller => "application", :action => "calculate_random" })
 
+  get("/square_root/new", { :controller => "application", :action => "blank_sqrt" })
+  get("/square_root/results", { :controller => "application", :action => "calculate_sqrt" })
 
-
+  get("/payment/new", { :controller => "application", :action => "blank_payment" })
+  get("/payment/results", { :controller => "application", :action => "calculate_payment" })
 end
